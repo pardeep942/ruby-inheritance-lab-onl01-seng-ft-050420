3fa9572fc822d1c763fbc9d6298cf37f300c9ee7
PARDEEP KAUR <pkaur84@aol.com>
2020-05-28 16:14:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
 
-end
+
+  attr_accessor :first_name, :last_name, :knowledge
+
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end 
+
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end 
+
+end 	
